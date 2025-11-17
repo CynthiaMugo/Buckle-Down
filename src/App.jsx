e14185699153c8b1c2b552e66682cd17dd854205
Cynthia Mugo <cynthiamugo25@gmail.com>
2025-11-17 20:20:16 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <p>
+        Let's build something amazing with React and Vite!
+      </p>
+
+      </div>
+      
+    </>
+  )
+}
+
+export default App
